d68b5b77b8b2a63eedd0a0e0b226190f178a41d4
you know who <chorvickprocode@gmail.com>
2021-03-22 12:57:01 -0400

    added Tag model

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -2,11 +2,20 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
-
+class Tag extends Model { }
+/// set up Tag col's as defined in assignment directions
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
