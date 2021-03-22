b7c04445b1307605b4c1e264a7555ff915db346a
you know who <chorvickprocode@gmail.com>
2021-03-22 12:36:39 -0400

    updated ProductTag to match assignment instructions

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,38 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// import our database connection from config.js
 const sequelize = require('../config/connection');
-
-class ProductTag extends Model {}
+const Product = require('./Product');
+// Initialize Product Tags  model (table) by extending off Sequelize's Model class
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    // define columns set up fields and rules for Product Tags model
+
+    id: {
+      type: DataTypes.INTEGER,
+      allownull: false,
+      primarykey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Product",
+        key: "id",
+
+      },
+
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+
+    }
   },
   {
     sequelize,
